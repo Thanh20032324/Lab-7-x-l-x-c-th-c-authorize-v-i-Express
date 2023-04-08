f663aba701157707c907908a3babf457c00f97dd
thanh <thanhttph23851@fpt.edu.vn>
2023-04-08 20:07:44 +0700

    update

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  secret: "nodeauthsecret",
+  database: "mongodb://127.0.0.1:27017/MOB402mongodb+srv://thanhttph23851:thanh2003@usermongo.ds8ek62.mongodb.net/user?retryWrites=true&w=majority",
+};
